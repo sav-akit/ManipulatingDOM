4aa1c5eeab7fc02ac22ee6566c2b50961783a83b
sav-akit <reddyssavitha@gmail.com>
2023-11-17 11:44:28 -0500

    manipulated the DOM

--- script.js
+++ script.js
@@ -17,32 +17,79 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+  const myMain = document.getElementById('main-title');
+  myMain.innerHTML = "Welcome to myDOM"
+  
 
   // Part 2
-
-
+  document.querySelector('body').style.backgroundColor = '#a4e0da';
+  
+  
   // Part 3
 
+  const favList = document.getElementById('favorite-things');
+  favList.removeChild(favList.lastElementChild);
+  //console.log(favList)
 
   // Part 4
-
-
+  const specialClass = document.querySelectorAll("special-title");
+  specialClass.forEach(element => {
+    element.style.fontSize = "2rem";
+  });
   // Part 5
 
+  const myElement = document.getElementById("past-races");
+  for (const child of myElement.children) {
+    if(child.textContent === "Chicago"){
+      myElement.removeChild(child);
+    }
+  }
 
   // Part 6
 
+  const listEl = document.createElement('li');
+  const textEl = document.createTextNode("Virginia");
+  listEl.appendChild(textEl);
+  //console.log(listEl);
+  myElement.appendChild(listEl);
 
   // Part 7
+  const newDiv = document.createElement('div');
+  newDiv.className = "blog-post purple";
+  const myHeader = document.createElement('h1');
+  const textHeader = document.createTextNode("Virginia");
+  myHeader.appendChild(textHeader);
+  const myPara = document.createElement('p');
+  const textPara = document.createTextNode("My car racing to Virginia..");
+  myPara.appendChild(textPara);
+   newDiv.appendChild(myHeader);
+   newDiv.appendChild(myPara);
+   
+  // const domAdv = document.getElementById("dom-adventures");
+  // const parent = domAdv.parentNode;
+  // parent.appendChild(newDiv);
 
-
+  const main = document.querySelector(".main")
+  main.appendChild(newDiv);
   // Part 8
 
+  // const randomQuote = function() {
+  //   document.querySelector('#quote-of-the-day').innerText = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
+  // };
+  document.getElementById("quote-title").addEventListener("click",randomQuote);
 
-  // Part 9
-
-
-
+   // Part 9
+  
+  const el = document.getElementsByClassName("blog-post");
+  console.log(el.length);
+  
+  for(let i = 0; i < el.length; i++) {
+    el[i].addEventListener("mouseout",function(){
+           el[i].classList.toggle("purple");
+         });
+    el[i].addEventListener("mouseenter",function(){
+          el[i].classList.toggle("red");
+         })
+  }
 
 });
